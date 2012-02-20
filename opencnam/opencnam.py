d0db25184e2f82cb542dd5de1df3cd36291eb07e
Randall Degges <rdegges@gmail.com>
2012-02-20 15:25:31 -0800

    Adding skeleton opencnam.opencnam module.
    This will hold the bulk of our code.

---
+++ opencnam/opencnam.py
@@ -0,0 +1,12 @@
+"""A simple python library for getting caller ID name information."""
+
+
+__author__ = 'Randall Degges'
+__copyright__ = 'Telephony Research Services, LLC'
+__credits__ = ('Randall Degges', )
+
+__license__ = 'UNLICENSE'
+__version = '0.1'
+__maintainer__ = 'Randall Degges'
+__email__ = 'rdegges@gmail.com'
+__status__ = 'Development'
