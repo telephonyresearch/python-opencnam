40c4861d8a5cda102a4691ee64561b58444d756e
Randall Degges <rdegges@gmail.com>
2012-02-25 18:25:32 -0800

    Implementing ``Phone._parse_number()``.
    This method returns a cleaned up phone number. This way, users can pass any
    format of phone number they want to us, and we'll clean it up into 10-digit
    format and return it (if possible).
    
    Otherwise, we'll raise an exception.

--- opencnam/opencnam.py
+++ opencnam/opencnam.py
@@ -12,6 +12,11 @@ __email__ = 'rdegges@gmail.com'
 __status__ = 'Development'
 
 
+from re import sub
+
+from .errors import InvalidPhoneNumberError
+
+
 class Phone(object):
     """An object that holds a single phone's information.
 
@@ -22,3 +27,27 @@ class Phone(object):
     def __init__(self, number):
         self.cnam = ''
         self.number = self._parse_number(str(number))
+
+    def _parse_number(self, number):
+        """Make ``number`` a valid phone number and return it.
+
+        :param str number: Phone number in any format.
+        :rtype: string
+        :returns: A valid 10-digit US phone number as a string.
+        :raises: InvalidPhoneNumberError if ``number`` cannot be made into a
+            valid US phone number.
+
+        Usage::
+
+            from opencnam import Phone
+
+            phone = Phone('+1-818-217-9229')
+            assert phone.number == '8182179229'
+        """
+        number = sub(r'\D', '', number)
+        number = number[-10:]
+
+        if len(number) < 10 or not (2002000000 <= long(number) <= 9999999999):
+            raise InvalidPhoneNumberError
+
+        return number
