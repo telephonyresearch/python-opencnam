294658cf9037f3fabbfd9ca4fdadc46374bf9fec
Randall Degges <rdegges@gmail.com>
2012-02-25 18:24:54 -0800

    Adding a skeleton class, Phone, to opencnam.
    This class will hold our basic stuff.

--- opencnam/opencnam.py
+++ opencnam/opencnam.py
@@ -10,3 +10,15 @@ __version = '0.1'
 __maintainer__ = 'Randall Degges'
 __email__ = 'rdegges@gmail.com'
 __status__ = 'Development'
+
+
+class Phone(object):
+    """An object that holds a single phone's information.
+
+    :attr str number: The validated 10-digit US phone number.
+    :attr str cnam: The caller ID name for this phone.
+    """
+
+    def __init__(self, number):
+        self.cnam = ''
+        self.number = self._parse_number(str(number))
