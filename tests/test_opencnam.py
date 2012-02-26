140b97686ec19d7617d1005c9849eb57cc290f8d
Randall Degges <rdegges@gmail.com>
2012-02-25 18:27:24 -0800

    Adding some basic unit tests.
    These exercise ``opencnam.Phone``.

---
+++ tests/test_opencnam.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+from opencnam import InvalidPhoneNumberError, Phone
+
+
+class Phonetest(TestCase):
+
+    def test_init_requires_number(self):
+        self.assertRaises(TypeError, Phone)
+
+    def test_init_sets_empty_cnam(self):
+        phone = Phone('8182179229')
+        self.assertEqual(phone.cnam, '')
+
+    def test_init_sets_number_after_validation(self):
+        phone = Phone('8182179229')
+        self.assertEqual(phone.number, '8182179229')
+
+    def test_parse_number_removes_non_ints(self):
+        phone = Phone('8182179229a')
+        self.assertEqual(phone.number, '8182179229')
+
+        phone = Phone('a8182179229')
+        self.assertEqual(phone.number, '8182179229')
+
+        phone = Phone('   818   217   -9229')
+        self.assertEqual(phone.number, '8182179229')
+
+    def test_parse_number_raises_error_on_short_number(self):
+        self.assertRaises(InvalidPhoneNumberError, Phone, '818217922')
+
+    def test_parse_returns_only_last_10_digits_of_number(self):
+        phone = Phone('11111111111111118182179229')
+        self.assertEqual(phone.number, '8182179229')
+
+    def test_parse_number_returns_valid_number_untouched(self):
+        phone = Phone('8182179229')
+        self.assertEqual(phone.number, '8182179229')
+
+    def test_parse_number_catches_invalid_numbers(self):
+        self.assertRaises(InvalidPhoneNumberError, Phone, '1001000000')
+        self.assertRaises(InvalidPhoneNumberError, Phone, '2001999999')
