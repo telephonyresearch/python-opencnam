5d325076e0e86cae2779ad5723a2818dc899a8fd
Randall Degges <rdegges@baal.(none)>
2012-02-26 16:58:10 -0800

    Adding custom API class (based on slumber.API).
    This is to fix a bug in Slumber, whose pull request hasn't yet been accepted.
    See: https://github.com/dstufft/slumber/pull/12 for more information.

---
+++ opencnam/helpers.py
@@ -0,0 +1,19 @@
+"""Some hack-ish helpers."""
+
+
+from requests import session
+from slumber.api import API as SlumberAPI
+
+
+class API(SlumberAPI):
+    """A temporary wrapper around Slumber's API class. This is due to
+    https://github.com/dstufft/slumber/pull/12.
+
+    Once this pull request is merged (and the bug fixed), I'll revert to using
+    Slumber's native API class.
+    """
+    def __init__(self, base_url=None, auth=None, format=None,
+            append_slash=True):
+        super(API, self).__init__(base_url, auth, format, append_slash)
+        self._store['session'] = session(auth=auth, headers={'Accept':
+            'application/json'})
