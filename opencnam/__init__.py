bd053e69280e5c4deb8e39dfd589457d0d80b8ea
Randall Degges <rdegges@baal.(none)>
2012-02-26 17:15:13 -0800

    Moving all metadata to the package level (__init__).

--- opencnam/__init__.py
+++ opencnam/__init__.py
@@ -1,2 +1,16 @@
+"""A simple python library for getting caller ID name information."""
+
+
 from .errors import *
 from .opencnam import *
+
+
+__author__ = 'Randall Degges'
+__copyright__ = 'Telephony Research Services, LLC'
+__credits__ = ('Randall Degges', )
+
+__license__ = 'UNLICENSE'
+__version = '0.1'
+__maintainer__ = 'Randall Degges'
+__email__ = 'rdegges@gmail.com'
+__status__ = 'Development'
